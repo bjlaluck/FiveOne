9d8f7b27aa2bac385656364dd4f405d96aea47a2
Bohdan Laluck <bohdanlaluck@hotmail.com>
2018-09-30 19:21:59 -0400

    5.2 sendgrid added

---
+++ app/controllers/simple_pages_controller.rb
@@ -0,0 +1,31 @@
+class SimplePagesController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def landing_page
+    @products = Product.all
+  end
+
+  def admin
+
+    @product = Product.first
+
+  end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'your-email@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
+
+end
